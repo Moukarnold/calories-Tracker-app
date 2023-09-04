e88c71c693c60d3f0eba02a8ea6e4ca4bbb2c1bf
Moukarnold <moukarnold@gmail.com>
2023-09-04 12:24:38 +0200

    save Meals to local storage and putt all eventlistenerss to _loadeventlisteners

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ class CalorieTracker{
    constructor(){
     this._calorieLimit= Storage.getCalorieLimit();
     this._totalCalories= Storage.getTotalCalories(0);
-    this._meals= [];
+    this._meals= Storage.getMeals();
     this._workouts= [];
     
     this._displayCaloriesLimit();
@@ -19,6 +19,7 @@ class CalorieTracker{
   this._meals.push(meal);
   this._totalCalories += meal.calories;
   Storage.updateTotalCalories(this._totalCalories);
+   Storage.saveMeal(meal);
   this._displayNewMeal(meal);
   this._render();
 }
@@ -70,6 +71,10 @@ setLimit(calorieLimit){
   this._displayCaloriesLimit();
   this._render();
   
+}
+
+loadItems(){
+  this._meals.forEach(meal=>this._displayNewMeal(meal));
 }
 
 // private methods
@@ -240,6 +245,23 @@ static getTotalCalories(defaultCalories = 0){
      localStorage.setItem("totalCalories", calories);
  }
 
+static getMeals(){
+  let meals;
+  if(localStorage.getItem("meals") === null){
+    meals= [];
+  }else{
+    meals= JSON.parse(localStorage.getItem("meals"));
+  }
+  return meals;
+}
+
+  static saveMeal(meal){
+  const meals= Storage.getMeals();
+  meals.push(meal);
+  localStorage.setItem("meals", JSON.stringify(meals));
+
+  }
+
  }
   
 
@@ -247,7 +269,12 @@ static getTotalCalories(defaultCalories = 0){
   class App{
 
 constructor(){
-  this._tracker= new CalorieTracker;
+  this._tracker= new CalorieTracker();
+   this._loadEventListeners();
+    this._tracker.loadItems();
+
+_loadEventListeners(){ 
+
   document.getElementById("meal-form").addEventListener("submit", this._newMeal.bind(this));
   document.getElementById("workout-form").addEventListener("submit", this._newWorkout.bind(this));
 
@@ -262,7 +289,7 @@ constructor(){
 
    document.getElementById("limit-form").addEventListener("submit", this._setLimit.bind(this));
 
-
+}
 
   }
 
